f6b6ab76a66897456a0b9fa765c7f5fa9d2f6401
zmokokokok <36779796+zmokokokok@users.noreply.github.com>
2020-05-07 16:22:22 +0800

    Update submit.py

--- Eddy_Detection_Tracking/submit.py
+++ Eddy_Detection_Tracking/submit.py
@@ -1,7 +1,7 @@
 from pypai import PAI
 
 # Create a PAI cluster
-pai = PAI(username='zm', passwd='123456')
+pai = PAI(username=, passwd=)
 
 # Generate the configuration
 pai.submit(exclude_file='.h5')
